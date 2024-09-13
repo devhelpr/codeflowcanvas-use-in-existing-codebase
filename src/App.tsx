014084d520fe8cbe66860ca5ca00a36b86f555b1
Maikel van de Lisdonk <maikel@devhelpr.com>
2024-09-13 18:58:11 +0200

    code-flow-canvas in existing react code-base

--- src/App.tsx
+++ src/App.tsx
@@ -22,37 +22,37 @@ function App() {
   );
 
   return (
-    <>
-      <p>Celsius to Fahrenheit using codeflowcanvas</p>
-      <div className="w-[clamp(240px,100%,1024px)] mx-auto">
-        <div className="flex flex-col">
-          <label>Celcius</label>
-          <input
-            className="border border-solid border-black "
-            name="celsius"
-            value={celsius}
-            onChange={(event: ChangeEvent<HTMLInputElement>) => {
-              setCelsius(event.target.value);
-              const input = event.target as HTMLInputElement;
-              console.log("celsius input", input.value);
-              sendMessageToNode(endpoints.celsius, input.value);
-            }}
-          />
-          <label>Fahrenheit</label>
-          <input
-            className="border border-solid border-black"
-            name="fahrenheit"
-            value={fahrenheit}
-            onChange={(event: ChangeEvent<HTMLInputElement>) => {
-              setFahrenheit(event.target.value);
-              const input = event.target as HTMLInputElement;
-              console.log("fahrenheit input", input.value);
-              sendMessageToNode(endpoints.fahrenheit, input.value);
-            }}
-          />
-        </div>
+    <div className="w-[clamp(240px,100%,65ch)] mx-auto text-left">
+      <h1 className="text-4xl text-bold text-balance">
+        Celsius to Fahrenheit using codeflowcanvas
+      </h1>
+      <div className="flex flex-col">
+        <label>Celcius</label>
+        <input
+          className="border border-solid border-black "
+          name="celsius"
+          value={celsius}
+          onChange={(event: ChangeEvent<HTMLInputElement>) => {
+            setCelsius(event.target.value);
+            const input = event.target as HTMLInputElement;
+            console.log("celsius input", input.value);
+            sendMessageToNode(endpoints.celsius, input.value);
+          }}
+        />
+        <label>Fahrenheit</label>
+        <input
+          className="border border-solid border-black"
+          name="fahrenheit"
+          value={fahrenheit}
+          onChange={(event: ChangeEvent<HTMLInputElement>) => {
+            setFahrenheit(event.target.value);
+            const input = event.target as HTMLInputElement;
+            console.log("fahrenheit input", input.value);
+            sendMessageToNode(endpoints.fahrenheit, input.value);
+          }}
+        />
       </div>
-    </>
+    </div>
   );
 }
 
